65b122a3b293d264f9fa4d2af70ea8e400b9733a
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-08-27 09:09:30 +0300

    Add v2 solution

--- Lesson_3/Task_1.py
+++ Lesson_3/Task_1.py
@@ -5,5 +5,8 @@ l = []
 for i in range(len(my_list)):
     if i % 2 != 0:
         l.append(my_list[i])
-
 print(f'Summ of items standing in an odd position is : {(sum(l))}')
+
+# v2
+list_one = [1, 10, 2, 4, 1, 6]
+print(f'Summ of items standing in an odd position is : {(sum(list_one[1::2]))}')
