1c0924e36da0908b2f5f5dc3116c34335270a8a0
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-09-16 17:33:00 +0300

    Refactoring

--- Homework_4/Task_2.py
+++ Homework_4/Task_2.py
@@ -1,13 +1,11 @@
 # Write a program that outputs a list of non-repeating elements of the original sequence
 
-num_list = [1, 2, 3, 5, 1, 5, 3, 10]
+num_list = [x for x in range(13)]
 
 
 def get_unique_numbers(numbers):
     list_of_unique_numbers = []
-    unique_numbers = set(numbers)
-
-    for number in unique_numbers:
+    for number in set(numbers):
         list_of_unique_numbers.append(number)
 
     return list_of_unique_numbers
